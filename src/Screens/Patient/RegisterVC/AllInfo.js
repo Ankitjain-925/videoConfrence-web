bc78f5365e7c5e9562c5c56f49b89c86e095e11c
12345AmanJain <aman.webnexus@gmail.com>
2022-09-20 11:35:49 +0530

    dark mode topup and some validation

--- src/Screens/Patient/RegisterVC/AllInfo.js
+++ src/Screens/Patient/RegisterVC/AllInfo.js
@@ -140,7 +140,8 @@ class AllInfo extends Component {
                                                         <h5 className="selectdoc-head"> {'('}{this.props.dataa.doctor_detail[0].profile_id}{')'}</h5>
                                                         {/* <p className='selectdoc-content'>Thu, Feb 3-8:30 am EST</p> */}
                                                         <Grid className='selectdoc-button'>
-                                                            <span>on-line</span>
+                                                        <img className = "v_c_img" src={require('assets/images/video-call-copy2.svg')} alt="" title="" />on-line
+
 
                                                         </Grid>
                                                     </Grid>
@@ -174,7 +175,8 @@ class AllInfo extends Component {
                                                                     <h5 className="selectdoc-head"> {'('}{item.profile_id}{')'}</h5>
                                                                     {/* <p className='selectdoc-content'>Thu, Feb 3-8:30 am EST</p> */}
                                                                     <Grid className='selectdoc-button'>
-                                                                        <span>on-line</span>
+                                                                    <img className = "v_c_img" src={require('assets/images/video-call-copy2.svg')} alt="" title="" />on-line
+
 
                                                                     </Grid>
                                                                 </Grid>
