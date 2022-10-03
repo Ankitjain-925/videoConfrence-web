7ee14d97d023761d649f60f5fa00e2d208112ebc
Tanyawebnexus <tanya.webnexus@gmail.com>
2022-10-03 05:22:45 -0700

    worked on jira tickets

--- src/Screens/Patient/SickLeaveForm/api.js
+++ src/Screens/Patient/SickLeaveForm/api.js
@@ -2084,7 +2084,7 @@ export const validatePainHeart = (check, value, item, current) => {
       } 
       else if(!value.headache_situation){
         current.setState({
-          error_section: 58,
+          error_section: 85,
           errorChrMsg: please_select_situation,
         });
         MoveTop(200);
