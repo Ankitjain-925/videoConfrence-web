ecdee56ed6f6a847612f862a7cbf179b006b6088
Tanyawebnexus <tanya.webnexus@gmail.com>
2022-10-07 11:25:15 +0530

    ticket 88 work

--- src/Screens/Patient/RegisterVC/Main.js
+++ src/Screens/Patient/RegisterVC/Main.js
@@ -3,7 +3,7 @@ import PersonalInfo from './PersonalInfo';
 import JobDetails from './JobDetails';
 import AllInfo from './AllInfo';
 import Step4 from './Step4';
-import SelectDoctor from '../Dashboard/selectdoctor';
+import SelectDoctor from '../Dashboard/Selectdoctors';
 import Form5 from '../SickLeaveForm/index'
 
 import Grid from "@material-ui/core/Grid";
