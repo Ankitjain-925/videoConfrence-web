6d75fc69aa6e84423be9d6130e09e787efa3d0cb
Ankitjainwebnexus <ankitjain.webnexus@gmail.com>
2023-01-10 16:40:45 +0530

    For Taking pull

--- src/sitedata.js
+++ src/sitedata.js
@@ -1,9 +1,9 @@
 var data = {
-  // "path": "http://localhost:5000/api/v4",
+  "path": "http://localhost:5001/api/v5",
   // "live_site"  : "https://aimedis.io/",
   // "path"       : "https://aidoc.io/api/v3",
   // "path": "https://sys.aimedis.io/api/v3",
-  "path": 'https://virtualhospital.aidoc.io/api/v4',
+  // "path": 'https://virtualhospital.aidoc.io/api/v4',
   // "path": "https://aimedix.com/api/v4"
 };
 exports.data = data;
